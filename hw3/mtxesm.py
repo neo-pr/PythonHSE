dc8c5c170f8877b0623c43caab7599d03b8ea863
neo-pr <neo_sm@hotmail.com>
2025-03-31 04:58:47 +0300

    corrections task 3_2

--- hw3/mtxesm.py
+++ hw3/mtxesm.py
@@ -6,8 +6,12 @@ def validate_mtx(matriz):
     # validates dimensions of MATRIZ nxm
     # if valid returns the number of lines and columns
 
-    if isinstance(matriz, (int, float, complex, np.number)) or (isinstance(matriz, np.ndarray) and not matriz.shape):
-        raise ValueError("A matrix should have lines and columns. Only a single value found.")
+    if isinstance(matriz, (int, float, complex, np.number)) or (
+        isinstance(matriz, np.ndarray) and not matriz.shape
+    ):
+        raise ValueError(
+            "A matrix should have lines and columns. Only a single value found."
+        )
     lines = len(matriz)
     if not lines:
         raise ValueError("Invalid matrix! No values found.")
@@ -16,12 +20,16 @@ def validate_mtx(matriz):
     column = len(matriz[0])
     for i, line in enumerate(matriz[1:]):
         if len(line) != column:
-            raise ValueError(f"Not valid matrix! Invalid number of elements for column {i+2}")
-    return lines, i + 2   # i + 2 is the number of columns
+            raise ValueError(
+                f"Not valid matrix! Invalid number of elements for column {i + 2}"
+            )
+    return lines, i + 2  # i + 2 is the number of columns
+
 
 def assert_same_mtx_size(a, b):
-    assert a == b, \
-            f"Sizes of matrices should be equal, but got {a} (left) and {b} (right)."
+    assert a == b, (
+        f"Sizes of matrices should be equal, but got {a} (left) and {b} (right)."
+    )
 
 
 class Matrib:
@@ -30,27 +38,27 @@ class Matrib:
         self.content = matriz
         # validation
         lines, columns = validate_mtx(matriz)
-        
+
         self.lines = lines
         self.columns = columns
         self.size = (lines, columns)
 
     def __add__(self, nova_matriz):
         # method for sum +
-        
+
         result = []
         # check compatibility
         assert_same_mtx_size(self.size, nova_matriz.size)
-        
+
         for i in range(self.size[0]):
             result.append([])
             for j in range(self.size[1]):
                 result[i].append(self.content[i][j] + nova_matriz.content[i][j])
-        return Matrib(result)
+        return self.__class__(result)
 
     def __mul__(self, nova_matriz):
         # method for multiplication *
-        
+
         result = []
         # check compatibility
         assert_same_mtx_size(self.size, nova_matriz.size)
@@ -59,29 +67,28 @@ class Matrib:
             result.append([])
             for j in range(self.size[1]):
                 result[i].append(self.content[i][j] * nova_matriz.content[i][j])
-        return Matrib(result)
-    
+        return self.__class__(result)
 
     def __matmul__(self, nova_matriz):
         # method for matrix multiplication @
 
         result = []
         # check compatibility
-        assert self.columns == nova_matriz.lines, \
-            "columns from the left matrix should be the same number of lines of the right matrix, but got " \
+        assert self.columns == nova_matriz.lines, (
+            "columns from the left matrix should be the same number of lines of the right matrix, but got "
             f" {self.columns} columns (left) and {nova_matriz.lines} lines (right)"
-        
+        )
+
         for i in range(self.lines):
             result.append([0] * self.columns)
-            for c in range(self.columns):
-                for l in range(nova_matriz.lines):
-                    result[i][c] += self.content[i][l] * nova_matriz.content[l][c]
-        return Matrib(result)
-    
-    def __str__(self):
+            for j in range(self.columns):
+                for ln in range(nova_matriz.lines):
+                    result[i][j] += self.content[i][ln] * nova_matriz.content[ln][j]
+        return self.__class__(result)
 
+    def __str__(self):
         # find max for width
-        width = 1   # min width
+        width = 1  # min width
         values = []
         for i in range(self.lines):
             for j in range(self.columns):
@@ -91,17 +98,18 @@ class Matrib:
                 values.append(value)
                 if width < (m := len(str(value))):
                     width = m
-        space_bound = self.columns * (width + 1) - 1    # upper and lower bounds
+        space_bound = self.columns * (width + 1) - 1  # upper and lower bounds
         result = f"┌ {' ' * space_bound} ┐\n"
         seq = 0
         for i in range(self.lines):
             result += "│"
             for j in range(self.columns):
-                result += f" {values[seq+j]:>{width}}"
+                result += f" {values[seq + j]:>{width}}"
             seq += j + 1
             result += " │\n"
         result += f"└ {' ' * space_bound} ┘"
         return result
+
 
 def main():
     np.random.seed(0)
@@ -111,7 +119,11 @@ def main():
     file2 = pathlib.Path("artifacts/3_1_matrix_mul.txt")
     file3 = pathlib.Path("artifacts/3_1_matrix_matmul.txt")
 
-    with open(file1, "w", encoding="utf-8") as f1, open(file2, "w", encoding="utf-8") as f2, open(file3, "w", encoding="utf-8") as f3:
+    with (
+        open(file1, "w", encoding="utf-8") as f1,
+        open(file2, "w", encoding="utf-8") as f2,
+        open(file3, "w", encoding="utf-8") as f3,
+    ):
         f1.write(str(a + b))
         f2.write(str(a * b))
         f3.write(str(a @ b))
--- hw3/mtzesm.py
+++ hw3/mtzesm.py
@@ -1,13 +1,18 @@
-import numpy as np
+import numbers
 import pathlib
+import numpy as np
 
 
 def validate_mtx(matriz):
     # validates dimensions of MATRIZ nxm
     # if valid returns the number of lines and columns
 
-    if isinstance(matriz, (int, float, complex, np.number)) or (isinstance(matriz, np.ndarray) and not matriz.shape):
-        raise ValueError("A matrix should have lines and columns. Only a single value found.")
+    if isinstance(matriz, (int, float, complex, np.number)) or (
+        isinstance(matriz, np.ndarray) and not matriz.shape
+    ):
+        raise ValueError(
+            "A matrix should have lines and columns. Only a single value found."
+        )
     lines = len(matriz)
     if not lines:
         raise ValueError("Invalid matrix! No values found.")
@@ -16,62 +21,76 @@ def validate_mtx(matriz):
     column = len(matriz[0])
     for i, line in enumerate(matriz[1:]):
         if len(line) != column:
-            raise ValueError(f"Not valid matrix! Invalid number of elements for column {i+2}")
-    return lines, i + 2   # i + 2 is the number of columns
+            raise ValueError(
+                f"Not valid matrix! Invalid number of elements for column {i + 2}"
+            )
+    return lines, i + 2  # i + 2 is the number of columns
+
 
 def assert_same_mtx_size(a, b):
-    assert a == b, \
-            f"Sizes of matrices should be equal, but got {a} (left) and {b} (right)."
+    assert a == b, (
+        f"Sizes of matrices should be equal, but got {a} (left) and {b} (right)."
+    )
 
 
 class Matrib(object):
-    def __init__(self, matriz):
-        super().__init__()
-        self.content = matriz
+    def __init__(self):
+        self._content = None
 
     @property
     def size(self):
         return self._size
-    
+
     @size.setter
     def size(self, val):
         raise NotImplementedError("Reshaping is not implemented")
-  
-    def __str__(self):
 
+    def __str__(self):
         # find max for width
-        width = 1   # min width
+        width = 1  # min width
         values = []
         for i in range(self._lines):
             for j in range(self._columns):
-                value = self.content[i][j]
-                if isinstance(value, (float, np.float32, np.float64)):
-                    value = round(value, 3)
-                values.append(value)
-                if width < (m := len(str(value))):
+                number = self._content[i][j]
+                if isinstance(number, (float, np.float32, np.float64)):
+                    number = round(number, 3)
+                values.append(number)
+                if width < (m := len(str(number))):
                     width = m
-        space_bound = self._columns * (width + 1) - 1    # upper and lower bounds
+        space_bound = self._columns * (width + 1) - 1  # upper and lower bounds
         result = f"┌ {' ' * space_bound} ┐\n"
         seq = 0
         for i in range(self._lines):
             result += "│"
             for j in range(self._columns):
-                result += f" {values[seq+j]:>{width}}"
+                result += f" {values[seq + j]:>{width}}"
             seq += j + 1
             result += " │\n"
         result += f"└ {' ' * space_bound} ┘"
         return result
-    
-    def __setattr__(self, name, value):
-        self.__dict__[name] = value
 
-        if name == "content":
-            # validation
-            lines, columns = validate_mtx(value)
-            # setting more attributes
-            self.__dict__["_lines"] = lines
-            self.__dict__["_columns"] = columns
-            self.__dict__["_size"] = (lines, columns)
+    def save(self, file_path):
+        if isinstance(file_path, str):
+            file_path = pathlib.Path(file_path)
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(self.__str__())
+
+    @property
+    def value(self):
+        return self._content
+
+    @value.setter
+    def value(self, value):
+        # validation
+        lines, columns = validate_mtx(value)
+
+        # setting the attribute
+        self._content = value
+
+        # setting extra attributes
+        self._lines = lines
+        self._columns = columns
+        self._size = (lines, columns)
 
     def __getattribute__(self, name):
         if name == "size":
@@ -79,11 +98,48 @@ class Matrib(object):
         return object.__getattribute__(self, name)
 
     def __getattr__(self, name):
-        raise AttributeError(f"There is no '{name}' attribute. "
-                             "There are only two attributes: 'size' and 'content'")
+        raise AttributeError(
+            f"There is no '{name}' attribute. "
+            "There are only two attributes: 'size' and 'content'"
+        )
 
 
-class Matriz(Matrib, np.lib.mixins.NDArrayOperatorsMixin):
+class ArrayLike(np.lib.mixins.NDArrayOperatorsMixin):
+    def __init__(self, value):
+        super().__init__()
+
+        self.value = np.asarray(value)
+
+    _HANDLED_TYPES = (np.ndarray, numbers.Number)
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+        out = kwargs.get("out", ())
+        for x in inputs + out:
+            if not isinstance(x, self._HANDLED_TYPES + (ArrayLike, Matriz)):
+                return NotImplemented
+
+        inputs = tuple(x.value if isinstance(x, ArrayLike) else x for x in inputs)
+        if out:
+            kwargs["out"] = tuple(
+                x.value if isinstance(x, ArrayLike) else x for x in out
+            )
+        result = getattr(ufunc, method)(*inputs, **kwargs)
+
+        if type(result) is tuple:
+            # multiple return values
+            return tuple(type(self)(x) for x in result)
+        elif method == "at":
+            # no return value
+            return None
+        else:
+            # one return value
+            return type(self)(result)
+
+    def __repr__(self):
+        return "%s(%r)" % (type(self).__name__, self.value)
+
+
+class Matriz(ArrayLike, Matrib):
     pass
 
 
@@ -91,15 +147,14 @@ def main():
     np.random.seed(0)
     a = Matriz(np.random.randint(0, 10, (10, 10)))
     b = Matriz(np.random.randint(0, 10, (10, 10)))
-    
-    file1 = pathlib.Path("artifacts/3_2_matrix_sum.txt")
-    file2 = pathlib.Path("artifacts/3_2_matrix_mul.txt")
-    file3 = pathlib.Path("artifacts/3_2_matrix_matmul.txt")
 
-    with open(file1, "w", encoding="utf-8") as f1, open(file2, "w", encoding="utf-8") as f2, open(file3, "w", encoding="utf-8") as f3:
-        f1.write(str(a + b))
-        f2.write(str(a * b))
-        f3.write(str(a @ b))
+    file1 = "artifacts/3_2_matrix_sum.txt"
+    file2 = "artifacts/3_2_matrix_mul.txt"
+    file3 = "artifacts/3_2_matrix_matmul.txt"
+
+    (a + b).save(file1)
+    (a * b).save(file2)
+    (a @ b).save(file3)
 
 
 if __name__ == "__main__":
